eb729cd9660ad153e3de860c9eb7ce4242ba8eda
ose0221 <ose0221@naver.com>
2025-04-17 19:51:30 +0900

    2주차: 과제 setting

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,74 +1,29 @@
 package org.sopt.controller;
 
-import org.sopt.domain.Post;
+import org.sopt.dto.PostRequest;
 import org.sopt.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
-
-import static org.sopt.validation.PostValidator.validateCoolTime;
-import static org.sopt.validation.PostValidator.validateTitleLength;
-
+@RestController
 public class PostController {
 
-    private final PostService postService = new PostService();
-    private LocalDateTime lastPostTime;
+    private final PostService postService;
 
-
-    public void createPost(String title) {
-        try {
-            // 입력 유효 시간 검사
-            validateCoolTime(lastPostTime);
-
-            // 글자수 제한 검사
-            validateTitleLength(title);
-
-            // 제목 중복 입력 검사
-            postService.validateDuplicateTitle(title);
-
-            postService.createPost(title);
-            lastPostTime = LocalDateTime.now();// 저장
-            System.out.println("✅ 게시글이 성공적으로 저장되었습니다!");
-        } catch (IllegalArgumentException | IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+    @PostMapping("/post")
+    public void createPost(@RequestBody final PostRequest postRequest) {
+        postService.createPost(postRequest.getTitle());
     }
 
-    public List<Post> getAllPosts() {
-        List<Post> postList = postService.getAllPosts();
-        if (postList.isEmpty()) {
-            System.out.println("📭 등록된 게시글이 없습니다.");
-        }
-        return postList;
+    @GetMapping("/posts")
+    public ResponseEntity<?> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
     }
-
-    public Post getPostById(int id) {
-        return postService.getPostById(id);
-    }
-
-    public boolean updatePostTitle(int id, String title) {
-        boolean result = false;
-        try {
-            validateTitleLength(title);
-            postService.validateDuplicateTitle(title);
-
-            result = postService.updatePost(id, title);
-            if(!result) {
-                System.out.println("❌ 해당 ID의 게시글이 존재하지 않습니다.");
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return result;
-    }
-
-    public Boolean deletePostById(int id) {
-        return postService.deletePostById(id);
-    }
-
-    public List<Post> searchPostsByKeyword(String keyword) {
-        return postService.searchPostsByKeyword(keyword);
-    }
-
 }
--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,71 +1,9 @@
 package org.sopt.repository;
 
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class PostRepository {
-    List<Post> postList = new ArrayList<>();
-
-    public void save(Post post) {
-        postList.add(post);
-    }
-
-    public List<Post> findAll() {
-        return postList;
-    }
-
-    public Post findOne(int id) {
-
-        for(Post post : postList) {
-            if(post.getId() == id) {
-                return post;
-            }
-        }
-        //존재하지 않는 id인 경우
-        return null;
-    }
-
-    public Boolean update(int id, String title) {
-        Post post = findOne(id);
-        if(post != null) {
-            post.setTitle(title);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public Boolean delete(int id) {
-        Post post = findOne(id);
-        if(post != null) {
-            postList.remove(post);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public List<Post> findByKeyword(String keyword) {
-            List<Post> searchPosts = new ArrayList<>();
-            if(keyword == null || keyword.trim().isEmpty()) {
-                return searchPosts;
-            }
-            for(Post post : postList) {
-                if(post.getTitle().contains(keyword)) {
-                    searchPosts.add(post);
-                }
-            }
-            return searchPosts;
-    }
-
-    public Boolean existsByTitle(String title) {
-        for(Post post : postList) {
-            if(post.getTitle().equals(title)) {
-                return true;
-            }
-        }
-        return false;
-    }
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
