82f3f95a51579a7f7810f7dbeb44074712a46341
ose0221 <ose0221@naver.com>
2025-04-20 18:42:14 +0900

    제목 입력 유효성(공백, 글자수 제한) 검증 기능 구현

--- src/main/java/org/sopt/exception/CommonException.java
+++ src/main/java/org/sopt/exception/CommonException.java
@@ -2,7 +2,7 @@ package org.sopt.exception;
 
 public enum CommonException {
     EMPTY_TITLE("❌ 제목은 비어있을 수 없습니다."),
-    OVER_LENGTH_TITLE("❌ 제목은 30자 미만이어야 합니다."),
+    OVER_LENGTH_TITLE("❌ 제목은 30자 이하로 작성해 주세요."),
     DUPLICATE_TITLE("❌ 이미 존재하는 제목입니다. 제목은 중복될 수 없습니다."),
     POST_DURATION("❌ 게시글은 3분에 1개만 작성할 수 있습니다.");
 
