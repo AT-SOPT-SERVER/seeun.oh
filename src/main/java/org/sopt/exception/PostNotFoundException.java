703c08c0b0cc57ddbfacbbc0712eb2fe52fde806
ose0221 <ose0221@naver.com>
2025-04-22 14:23:23 +0900

    중복된 errorCode 속성 삭제

--- src/main/java/org/sopt/exception/PostNotFoundException.java
+++ src/main/java/org/sopt/exception/PostNotFoundException.java
@@ -1,13 +1,8 @@
 package org.sopt.exception;
 
 public class PostNotFoundException extends CustomException {
-    private final ErrorCode errorCode;
 
     public PostNotFoundException(final ErrorCode errorCode) {
         super(errorCode);
-        this.errorCode = errorCode;
-    }
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
--- src/main/java/org/sopt/exception/TooManyRequestException.java
+++ src/main/java/org/sopt/exception/TooManyRequestException.java
@@ -1,14 +1,8 @@
 package org.sopt.exception;
 
 public class TooManyRequestException extends CustomException {
-    private final ErrorCode errorCode;
 
     public TooManyRequestException(ErrorCode errorCode) {
         super(errorCode);
-        this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
