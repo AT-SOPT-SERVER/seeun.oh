40ef6a56e8f5828360730be8517b30926cace06e
ose0221 <ose0221@naver.com>
2025-04-22 14:23:23 +0900

    중복된 errorCode 속성 삭제

--- src/main/java/org/sopt/exception/InvalidRequestException.java
+++ src/main/java/org/sopt/exception/InvalidRequestException.java
@@ -1,14 +1,8 @@
 package org.sopt.exception;
 
 public class InvalidRequestException extends CustomException {
-    private final ErrorCode errorCode;
 
     public InvalidRequestException(ErrorCode errorCode) {
         super(errorCode);
-        this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
