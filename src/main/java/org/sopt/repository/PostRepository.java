07753288e557514756b416107a9eec55c7a861fd
ose0221 <ose0221@naver.com>
2025-04-25 03:17:19 +0900

    Spring Data JPA 쿼리 메서드를 JPQL(@Query)로 변경

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -2,6 +2,7 @@ package org.sopt.repository;
 
 import org.sopt.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByTitleContaining(String keyword);
-    Optional<Post> findTopByOrderByCreatedAtDesc();
+    @Query("SELECT p FROM Post p WHERE p.title LIKE %:keyword%")
+    List<Post> searchByKeyword(String keyword);
+    @Query("SELECT p FROM Post p ORDER BY p.createdAt DESC")
+    Optional<Post> getLatestPost();
     boolean existsByTitle(String title);
 }
