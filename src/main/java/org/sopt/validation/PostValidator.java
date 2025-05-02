7d434a0a8a0ef7f392e2afd53c09324c90f6218d
ose0221 <ose0221@naver.com>
2025-05-02 17:33:02 +0900

    PostValidator 내용 글자수 제한 검증 추가

--- src/main/java/org/sopt/validation/PostValidator.java
+++ src/main/java/org/sopt/validation/PostValidator.java
@@ -6,15 +6,14 @@ import org.sopt.global.exception.TooManyRequestException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-import static org.sopt.global.common.ErrorCode.EMPTY_TITLE;
-import static org.sopt.global.common.ErrorCode.OVER_LENGTH_TITLE;
-import static org.sopt.global.common.ErrorCode.POST_DURATION;
+import static org.sopt.global.common.ErrorCode.*;
 import static org.sopt.util.GraphemeUtils.getLengthOfEmojiContainableText;
 
 public class PostValidator {
     private static final int MAX_TITLE_LENGTH = 30;
     private static final long COOL_TIME = 3;
     private static final long MIN_TO_SEC = 60;
+    private static final long MAX_CONTENT_LENGTH = 1000;
 
     public static void validateTitleLength(String title) {
         int count = getLengthOfEmojiContainableText(title);
@@ -38,6 +37,20 @@ public class PostValidator {
 
         if (duration.toSeconds() < timeLimit) {
             throw new TooManyRequestException(POST_DURATION);
+        }
+    }
+
+    public static void validateContentLength(String content) {
+        int contentCount = getLengthOfEmojiContainableText(content);
+
+        //입력이 비어있는 경우(공백) 검증
+        if(content == null || content.trim().isEmpty()) {
+            throw new InvalidRequestException(EMPTY_CONTENT);
+        }
+
+        //1000자 초과 검증
+        if(contentCount > MAX_CONTENT_LENGTH) {
+            throw new InvalidRequestException(OVER_LENGTH_CONTENT);
         }
     }
 
